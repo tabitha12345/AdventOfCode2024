9ca5256a0f7355b9c9b14426b9483a8d622dd3f8
tabitha12345 <tabby80@hotmail.com>
2024-12-09 15:08:39 -0400

    Day 1 - Add part 2

--- day01.py
+++ day01.py
@@ -1,12 +1,15 @@
 """
-Advent of code 2024 day 1, part 1
+Advent of code 2024 day 1
 """
+
+from collections import Counter
 
 # Set up the lists to be edited:
 col1 = []
 col2 = []
 diff = []
-
+frequency = {}
+similarity_score = []
 
 # Add each entry in each of the columns to a list:
 with open("inputs/day01.txt", "r") as file:
@@ -20,9 +23,6 @@ with open("inputs/day01.txt", "r") as file:
 col1.sort()
 col2.sort()
 
-print(col1)
-print(col2)
-
 # For each item in col1 and col2, find the difference and add it to diff:
 for item1, item2 in zip(col1, col2):
     difference = abs(int(item1) - int(item2))
@@ -31,5 +31,19 @@ for item1, item2 in zip(col1, col2):
 # Add all the items in diff together:
 total = sum(diff)
 
+# Find the frequency of each item in col1 within col2:
+col2_counter = Counter(col2)
+for item in col1:
+    frequency[item] = col2.count(item)
+
+# Calculate the similarity score for each item:
+for item in col1:
+    score = int(item) * frequency[item]
+    similarity_score.append(score)
+
+# Add all the similarity scores together:
+total_score = sum(similarity_score)
+
 # Print the result:
-print(str(total))
+print("Part 1: The total of the differences is " + str(total))
+print("Part 2: The similarity score is " + str(total_score))
