01d50921f6d34d18aeb3727c5ae35854513042a6
tabitha12345 <tabby80@hotmail.com>
2025-03-18 10:19:15 -0300

    Add day 3 part 2

--- day03.py
+++ day03.py
@@ -6,6 +6,7 @@ import re
 
 input_file = "inputs/day03.txt"
 total_sum = 0
+total_sum_2 = 0
 
 
 def find_and_multiply(input_file):
@@ -24,5 +25,32 @@ def find_and_multiply(input_file):
     return total_sum
 
 
+def find_and_multiply_conditional(input_file):
+    with open(input_file, "r") as file:
+        input_data = file.read()
+
+    # Find mul(X,Y) where X and Y are numbers
+    pattern_conditional = re.compile(r"(mul\((\d+),(\d+)\))|(do\(\))|(don't\(\))")
+    matches_conditional = pattern_conditional.findall(input_data)
+
+    total_sum_2 = 0
+    mul_enabled = True
+
+    for match in matches_conditional:
+        if match[1] and match[2]:
+            if mul_enabled:
+                x, y = int(match[1]), int(match[2])
+                total_sum_2 += x * y
+        elif match[3]:  # do() match
+            mul_enabled = True
+        elif match[4]:  # don't() match
+            mul_enabled = False
+
+    return total_sum_2
+
+
 result1 = find_and_multiply(input_file)
 print(f"Part 1: The sum of all valid multiplications is: {result1}")
+
+result2 = find_and_multiply_conditional(input_file)
+print(f"Part 2: The sum of all valid multiplications is: {result2}")
