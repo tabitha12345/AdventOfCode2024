b839f8a6d4e7271e7c234fee91899f77a80f41d0
tabitha12345 <tabby80@hotmail.com>
2025-06-17 12:34:03 -0300

    Add day 5 part 2

--- day05.py
+++ day05.py
@@ -2,9 +2,12 @@
 Advent of code 2024 day 5
 """
 
+from collections import defaultdict, deque
+
 input_file = "inputs/day05.txt"
 
 # Part 1:
+
 
 def parse_input(filename):
     with open(filename) as f:
@@ -46,7 +49,7 @@ def middle_value(update):
     return update[n // 2]
 
 
-def main():
+def part1():
     rules, updates = parse_input(input_file)
     rule_pairs = parse_rules(rules)
     updates = parse_updates(updates)
@@ -57,4 +60,46 @@ def main():
     print("The answer for Part 1 is: " + str(total))
 
 
-main()
+part1()
+
+# Part 2:
+
+
+def topological_sort(pages, rule_pairs):
+    adj = defaultdict(list)
+    indegree = defaultdict(int)
+    page_set = set(pages)
+    for x, y in rule_pairs:
+        if x in page_set and y in page_set:
+            adj[x].append(y)
+            indegree[y] += 1
+            indegree.setdefault(x, 0)
+    order = {p: i for i, p in enumerate(pages)}
+    queue = deque([p for p in pages if indegree[p] == 0])
+    result = []
+    while queue:
+        queue = deque(sorted(queue, key=lambda x: order[x]))
+        node = queue.popleft()
+        result.append(node)
+        for neighbor in adj[node]:
+            indegree[neighbor] -= 1
+            if indegree[neighbor] == 0:
+                queue.append(neighbor)
+    if len(result) != len(pages):
+        raise ValueError("Cycle detected or missing pages in topological sort")
+    return result
+
+
+def part2():
+    rules, updates = parse_input(input_file)
+    rule_pairs = parse_rules(rules)
+    updates = parse_updates(updates)
+    total = 0
+    for update in updates:
+        if not is_update_valid(update, rule_pairs):
+            sorted_update = topological_sort(update, rule_pairs)
+            total += middle_value(sorted_update)
+    print("The answer for Part 2 is: " + str(total))
+
+
+part2()
